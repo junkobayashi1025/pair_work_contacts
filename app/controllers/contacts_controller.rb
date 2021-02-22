59aafcd4ebc98c3c26125df39cfbdeb6a1f5b186
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-22 09:46:24 +0000

    done

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,14 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params.require(:contact).permit(:name, :email, :content))
+     if @contact.save
+       redirect_to new_contact_path
+     else
+       render :new
+     end
+  end
+end
